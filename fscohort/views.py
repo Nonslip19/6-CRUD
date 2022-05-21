5d411bd0d8cebb5f16711d9bdfec13b1a5272822
Mehmet <mkaymaz93@gmail.com>
2022-05-21 12:00:44 +0300

    first CRUD

---
+++ fscohort/views.py
@@ -0,0 +1,16 @@
+from django.shortcuts import render
+from .models import Student
+# Create your views here.
+from django.shortcuts import render
+
+def index(request):
+    return render(request, 'fscohort/index.html')
+
+
+def student_list(request):
+    students = Student.objects.all()
+
+    context = {
+        "students": students
+    }
+    return render(request, "fscohort/student_list.html", context)
